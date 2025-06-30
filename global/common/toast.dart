efb660679af1ee72ab22d84b7412ceddf534f60a
Jerrychantzis <jerrychantzis@gmail.com>
2025-06-30 19:46:11 +0300

    Add files via upload

---
+++ global/common/toast.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+void showToast({required String message}) {
+  Fluttertoast.showToast(
+      msg: message,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 2,
+      backgroundColor: Colors.redAccent,
+      textColor: Colors.white60,
+      fontSize: 16.0
+  );
+}
+
+void showToastGood({required String message}) {
+  Fluttertoast.showToast(
+      msg: message,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 1,
+      backgroundColor: Colors.blue,
+      textColor: Colors.white60,
+      fontSize: 16.0
+  );
+}
